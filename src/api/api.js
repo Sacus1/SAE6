daddebb11801a41c537ab10dffe7cb6f822550c8
Sacus <75908397+Sacus1@users.noreply.github.com.>
2024-02-22 11:39:14 +0100

    Add api key

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,10 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://ytpaqpikqarnveticqhl.supabase.co/rest/v1/';
-
+const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0cGFxcGlrcWFybnZldGljcWhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwNDQ3MzUsImV4cCI6MjAxOTYyMDczNX0.4glNGKdXcHAXUyWuO5fpvcmg4oRyH9TvtTZ7OYMkcfc';
 export const fetchTournees = async () => {
     try {
-        const response = await axios.post(`${BASE_URL}/tournees`);
+        const response = await axios.post(`${BASE_URL}/tournees`, { headers: { apikey: apiKey } });
         return response.data;
     } catch (error) {
         console.error('Failed to fetch tournees data:', error);
