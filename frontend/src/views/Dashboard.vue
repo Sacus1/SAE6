5458f16d1521042ab600c4f27f5214961947e949
ACBorgia <fakeilovewash@gmail.com>
2024-03-05 15:17:40 +0100

    ajoutStore et pinia

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,6 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+
+
+</script>
+
 <template>
     <div className="card">
-        <h5>Empty Page</h5>
-        <p>Use this page to start from scratch and place your custom content.</p>
+        <h5>Premiere page</h5>
+        <p>On va afficher ici les tournees</p>
     </div>
 </template>
