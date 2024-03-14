18ecd6a223c8f0ef74798fc3161d33707599dc54
Sacus <75908397+Sacus1@users.noreply.github.com.>
2024-03-14 19:35:14 +0100

    Optimisation de la récupération et du traitement des données pour la carte

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -12,52 +12,6 @@ export const fetchTournees = async () => {
         throw error;
     }
 };
-/*
-
-export const fetchDepotsByTourneeId = async (id) => {
-    try {
-        const distributions = await axios.get(`${BASE_URL}/distributions`, { headers: { apikey: apiKey }, params: { tournee_id: 'eq.' + id, select: 'depot_id' } });
-        let depots = [];
-        const depotIdsSet = new Set(distributions.data.map((item) => item.depot_id));
-        for (const depotId of depotIdsSet) {
-            const depot = await axios.get(`${BASE_URL}/depots`, { headers: { apikey: apiKey }, params: { depot_id: 'eq.' + depotId, select: 'depot' } });
-            const depotName = depot.data[0].depot;
-            const distributionsDepot = await axios.get(`${BASE_URL}/distributions`, { headers: { apikey: apiKey }, params: { depot_id: 'eq.' + depotId, select: 'distribution_id' } });
-            const distributionIdsSet = new Set(distributionsDepot.data.map((item) => item.distribution_id));
-            let nombreSimple = 0;
-            let nombreFamilial = 0;
-            let nombreFruite = 0;
-            for (const distributionId of distributionIdsSet) {
-                const livraisonsDistrib = await axios.get(`${BASE_URL}/livraisons`, { headers: { apikey: apiKey }, params: { distribution_id: 'eq.' + distributionId, select: 'abonnement_id' } });
-                const abonnementIdsSet = new Set(livraisonsDistrib.data.map((item) => item.abonnement_id));
-                for (let abonnementId of abonnementIdsSet) {
-                    const abonnementDistrib = await axios.get(`${BASE_URL}/abonnements`, { headers: { apikey: apiKey }, params: { abonnement_id: 'eq.' + abonnementId, select: 'panier_id,nombre' } });
-                    const panierId = abonnementDistrib.data[0].panier_id;
-                    const nombre = abonnementDistrib.data[0].nombre;
-                    //[1, 2, 5] : simple
-                    if (panierId == 1 || panierId == 2 || panierId == 5) nombreSimple += nombre;
-                    //[3, 4, 6] : familial
-                    if (panierId == 3 || panierId == 4 || panierId == 6) nombreFamilial += nombre;
-                    //[7, 8, 9] : fruite
-                    if (panierId == 7 || panierId == 8 || panierId == 9) nombreFruite += nombre;
-                }
-            }
-            const nombrePaniers = nombreSimple + nombreFamilial + nombreFruite;
-            depots.push({
-                depot: depotName,
-                nombrePaniers: nombrePaniers,
-                nombreSimple: nombreSimple,
-                nombreFruite: nombreFruite,
-                nombreFamilial: nombreFamilial
-            });
-        }
-        return depots;
-    } catch (error) {
-        console.error('Failed to fetch depots data:', error);
-        throw error;
-    }
-};
-*/
 export const fetchDepotsByTourneeId = async (tourneeId) => {
     try {
         const response = await axios.get(`${BASE_URL}/distributions`, {
@@ -80,7 +34,6 @@ export const fetchDepotsByTourneeId = async (tourneeId) => {
                 if ([1, 2, 5].includes(panier_id)) nombreSimple += nombre;
                 if ([3, 4, 6].includes(panier_id)) nombreFamilial += nombre;
                 if ([7, 8, 9].includes(panier_id)) nombreFruite += nombre;
-                console.log('panier_id:', panier_id, 'nombre:', nombre);
             });
 
             // Aggregate the total count for the depot
@@ -126,6 +79,37 @@ export const fetchAdresseById = async (id) => {
         return response.data;
     } catch (error) {
         console.error('Failed to fetch adresse data:', error);
+        throw error;
+    }
+};
+
+export const GetLocalisationsByTournee = async tourneeId => {
+    try {
+        // Hypothetical API call that fetches distributions with nested depot and address information
+        const response = await axios.get(`${BASE_URL}/distributions`, {
+            headers: { apikey: apiKey },
+            params: {
+                tournee_id: 'eq.' + tourneeId,
+                select: 'depots(depot_id,adresses(adresse_id,localisation))'
+            }
+        });
+        const distributions = response.data;
+        let points = [];
+
+        distributions.forEach(distribution => {
+            if (distribution.depots && distribution.depots.adresses) {
+                const adresse = distribution.depots.adresses;
+
+                // Assuming 'localisation' is a structure containing 'coordinates'
+                if (adresse.localisation && adresse.localisation.coordinates) {
+                    const [long, lat] = adresse.localisation.coordinates;
+                    points.push([lat, long]); // Adjust based on how your coordinates are structured
+                }
+            }
+        });
+        return points;
+    } catch (error) {
+        console.error('Optimized data fetch and processing failed:', error);
         throw error;
     }
 };
--- frontend/src/views/pages/Maps.vue
+++ frontend/src/views/pages/Maps.vue
@@ -1,139 +1,126 @@
 <script setup>
 import { nextTick, onMounted } from "vue";
 import { useRoute } from "vue-router";
+import { GetLocalisationsByTournee } from "@/api/api";
 import L from "leaflet";
 import "leaflet-routing-machine";
-import 'leaflet-polylinedecorator'
+import "leaflet-polylinedecorator";
 import { fetchAdresseById, fetchDepotById, fetchDistributionByTournee } from "@/api/api"; // Assuming you've installed and imported it
 const route = useRoute();
 
 onMounted(async () => {
-    await nextTick();
+  await nextTick();
   let points = [];
-    // create the map
-    const map = L.map('map');
+  // create the map
+  const map = L.map("map");
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
-    map.locate({ setView: true, maxZoom: 16 });
-    map.on('locationfound', (e) => {
-      const radius = e.accuracy / 2;
-      L.marker(e.latlng)
-        .addTo(map)
-        .bindPopup('You are within ' + radius + ' meters from this point')
-        .openPopup();
-      L.circle(e.latlng, radius).addTo(map);
-      points.push([e.latlng.lat, e.latlng.lng]);
-    });
-    map.on('locationerror', (e) => {
-      //console.log(e);
-    });
-    // get tournee id from the url
-    fetchDistributionByTournee(route.params.id).then(async (tournee) => {
-        if (tournee) {
-            for (let i = 0; i < tournee.length; i++) {
-                await fetchDepotById(tournee[i]['depot_id']).then(async (depot) => {
-                    if (depot) {
-                        depot = depot[0];
-                        await fetchAdresseById(depot['adresse_id']).then((adresse) => {
-                            adresse = adresse[0];
-                            if (adresse) {
-                                points.push([adresse['localisation']['coordinates'][1], adresse['localisation']['coordinates'][0]]);
-                            }
-                        });
-                    }
-                });
-            }
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap contributors"
+  }).addTo(map);
+  map.locate({ setView: true, maxZoom: 16 });
+  map.on("locationfound", (e) => {
+    const radius = e.accuracy / 2;
+    L.marker(e.latlng)
+      .addTo(map)
+      .bindPopup("You are within " + radius + " meters from this point")
+      .openPopup();
+    L.circle(e.latlng, radius).addTo(map);
+    points.push([e.latlng.lat, e.latlng.lng]);
+  });
+  map.on("locationerror", (e) => {
+    //console.log(e);
+  });
+  // get tournee id from the url
+  // sort the points
+  points = solveTSP(await GetLocalisationsByTournee(route.params.id));
+  console.log(points);
+
+  // Ensure the map instance is ready before adding routing
+  let routingControl = L.Routing.control({
+    waypoints: points.map((point) => L.latLng(point[0], point[1])),
+    plan: L.Routing.plan(
+      points.map((point) => L.latLng(point[0], point[1])),
+      {
+        createMarker: function(i, wp, nWps) {
+          return L.marker(wp.latLng, {
+            draggable: false,
+            addWaypoints: false,
+            icon: L.icon({
+              iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+              shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34],
+              shadowSize: [41, 41]
+            })
+          });
         }
-
-        // sort the points
-        points = solveTSP(points);
-
-
-        // Ensure the map instance is ready before adding routing
-      let routingControl =L.Routing.control({
-            waypoints: points.map((point) => L.latLng(point[0], point[1])),
-            plan: L.Routing.plan(
-                points.map((point) => L.latLng(point[0], point[1])),
-                {
-                    createMarker: function (i, wp, nWps) {
-                      return L.marker(wp.latLng, {
-                          draggable: false,
-                          addWaypoints: false,
-                          icon: L.icon({
-                            iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-                            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-                            iconSize: [25, 41],
-                            iconAnchor: [12, 41],
-                            popupAnchor: [1, -34],
-                            shadowSize: [41, 41]
-                          })
-                        });
-                    }
-                }
-            )
-        }).addTo(map);
-        routingControl.on('routesfound', function(e) {
-          let routes = e.routes;
-          let decorator = L.polylineDecorator(routes[0].coordinates, {
-            patterns: [
-              {offset: '0%', repeat: 50, symbol: L.Symbol.arrowHead({pixelSize: 15, polygon: false, pathOptions:
-                    {stroke: true}})}
-            ]
-          }).addTo(map);
-        });
-
-    });
+      }
+    )
+  }).addTo(map);
+  routingControl.on("routesfound", function(e) {
+    let routes = e.routes;
+    let decorator = L.polylineDecorator(routes[0].coordinates, {
+      patterns: [
+        {
+          offset: "0%", repeat: 50, symbol: L.Symbol.arrowHead({
+            pixelSize: 15, polygon: false, pathOptions:
+              { stroke: true }
+          })
+        }
+      ]
+    }).addTo(map);
+  });
 });
-function calculateDistance(a, b) {
-    // Haversine distance formula
-    const [lat1, lon1] = a;
-    const [lat2, lon2] = b;
-    const R = 6371; // Earth's radius in km
-    const dLat = (lat2 - lat1) * (Math.PI / 180);
-    const dLon = (lon2 - lon1) * (Math.PI / 180);
-    const lat1Rad = lat1 * (Math.PI / 180);
-    const lat2Rad = lat2 * (Math.PI / 180);
 
-    a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c; // Distance in km
+function calculateDistance(a, b) {
+  // Haversine distance formula
+  const [lat1, lon1] = a;
+  const [lat2, lon2] = b;
+  const R = 6371; // Earth's radius in km
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
+  const lat1Rad = lat1 * (Math.PI / 180);
+  const lat2Rad = lat2 * (Math.PI / 180);
+
+  a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c; // Distance in km
 }
 
 function findNearestNeighbor(currentPoint, points) {
-    return points.reduce(
-        (nearest, point, index) => {
-            const distance = calculateDistance(currentPoint, point);
-            return distance < nearest.distance ? { index, distance } : nearest;
-        },
-        { index: -1, distance: Infinity }
-    );
+  return points.reduce(
+    (nearest, point, index) => {
+      const distance = calculateDistance(currentPoint, point);
+      return distance < nearest.distance ? { index, distance } : nearest;
+    },
+    { index: -1, distance: Infinity }
+  );
 }
 
 function solveTSP(points) {
-    const path = [points[0]]; // Start from the first point
-    let currentPoint = points[0];
-    let remainingPoints = points.slice(1);
+  const path = [points[0]]; // Start from the first point
+  let currentPoint = points[0];
+  let remainingPoints = points.slice(1);
 
-    while (remainingPoints.length > 0) {
-        const nearest = findNearestNeighbor(currentPoint, remainingPoints);
-        path.push(remainingPoints[nearest.index]);
-        currentPoint = remainingPoints[nearest.index];
-        remainingPoints = remainingPoints.filter((_, index) => index !== nearest.index);
-    }
+  while (remainingPoints.length > 0) {
+    const nearest = findNearestNeighbor(currentPoint, remainingPoints);
+    path.push(remainingPoints[nearest.index]);
+    currentPoint = remainingPoints[nearest.index];
+    remainingPoints = remainingPoints.filter((_, index) => index !== nearest.index);
+  }
 
-    return path;
+  return path;
 }
 </script>
 <template>
-    <div className="card">
-        <h5>Carte livraison</h5>
-        <div id="map" style="height: 600px"></div>
-    </div>
+  <div className="card">
+    <h5>Carte livraison</h5>
+    <div id="map" style="height: 600px"></div>
+  </div>
 </template>
 <style>
 .leaflet-routing-container {
-    display: none !important;
+  display: none !important;
 }
 </style>
